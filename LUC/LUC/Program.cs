e4667f949ab9b419676edb5b7716ed5da88d1dc0
staffnerluca <113279240+staffnerluca@users.noreply.github.com>
2024-10-22 11:54:30 +0200

    Update Program.cs

--- LUC/LUC/Program.cs
+++ LUC/LUC/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
+            // TOOD Get input from file
             Lexer lex = new Lexer();
+            // inconsistent naming should be Lex in my opinion
             lex.DoLexer();
 
             SyntaxAnalysis analysis = new SyntaxAnalysis();
