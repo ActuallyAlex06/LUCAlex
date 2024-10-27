a183508811b7d043934580f746fbc4a8d150c84f
staffnerluca <113279240+staffnerluca@users.noreply.github.com>
2024-10-27 08:15:09 +0100

    started creating keywords.json

--- LUC/LUC/GetKeywords.cs
+++ LUC/LUC/GetKeywords.cs
@@ -22,7 +22,49 @@ public class GetKeywords
     // if the user accidentially deleted the keywords file a new one gets created
     public void GenerateStandardKeywordsFile()
     {
+        string filePath = "keywords.json";
+        if(!File.Exists(filePath))
+        {
+            Dictionary<string, List<string>> jsonData = new Dictionary<string, List<string>>(){
+                // functions
+                {"function", new List<string>{"func", "f", "fu"}},
+                {"pure", new List<string>{"p", "purefunction", "purefunc"}},
+                {"return", new List<string>{"r", "ret", "give_back"}},
+                {"filter", new List<string>{""}}, 
+                {"map", new List<string>{""}},
 
+
+                // Datastructures
+                {"int", new List<string>{"number", "num"}},
+                {"string", new List<string>{"chars"}},
+                {"bool", new List<string>{"b", "boolean", "trueOrFalse"}},
+                {"decimal", new List<string>{"double", "float", "dec"}},
+                {"[string]", new List<string>{"strings"}},
+                {"[int]", new List<string>{"ints", "numbers", "[num]"}},
+                {"[bool]", new List<string>{"bools"}},
+                
+                // Logical and comparision operator
+                {"==", new List<string>{"=", "equal", "equ", "eq"}},
+                {"!=", new List<string>{"n_eq", "not_equal", "no_eq", "no_equ", "n_e"}},
+                {">", new List<string>{"greater", "larger"}},
+                {"<", new List<string>{"smaller"}},
+                {">=", new List<string>{"greater_or_equal", "goe", "g_o_e"}},
+                {"<=", new List<string>{"smaller_or_equal", "soe"}},
+                {"and", new List<string>{"too"}},
+                {"or", new List<string>{""}},
+                
+                // Variables
+                {":=", new List<string>{"is"}},
+
+                // Loops
+                {"for", new List<string>{""}},
+                {"while", new List<string>{"wh", "whil", "during"}},
+
+                // Builtin functions
+                {"print", new List<string>{"write_line"}},
+            };
+            File.WriteAllText(filePath, JsonSerializer.Serialize(jsonData, new JsonSerializerOptions {WriteIndented = true}));
+        }
     }
 
     public List<string> GetSynonyms(string keyword)
